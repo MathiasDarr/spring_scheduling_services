f0600a42fe23702c0143e84bc73da5bf3d69edeb
mddarr <mddarr@gmail.com>
2020-12-12 19:13:18 -0800

    Refactor the script

--- data_model/populate_patients_postgres_table.py
+++ data_model/populate_patients_postgres_table.py
@@ -1,11 +1,11 @@
+"""
+Create & populate patients, providers tables
+"""
+# !/usr/bin/env python3
+
+
 import psycopg2
 import csv
-def get_postgres_connection():
-    conn = psycopg2.connect(host="localhost", port="5432", user="postgres", password="postgres", database="postgres")
-    return conn
-
-conn = get_postgres_connection()
-cur = conn.cursor()
 
 
 create_patients_table = """
@@ -17,9 +17,6 @@ create_patients_table = """
         );
 """
 
-cur.execute(create_patients_table)
-conn.commit()
-
 create_providers_table = """
         CREATE TABLE providers (
                 provider_id VARCHAR(50) PRIMARY KEY,
@@ -30,39 +27,35 @@ create_providers_table = """
         );
 """
 
-
-cur.execute(create_providers_table)
-conn.commit()
-
+insert_into_patients_table = """INSERT INTO patients(patient_id, first_name, last_name, age) VALUES(%s,%s,%s, %s);"""
+insert_into_providers_table = """INSERT INTO providers(provider_id, first_name, last_name, title, department) VALUES(%s,%s,%s,%s,%s);"""
 
 
+def populate_tables():
+
+    conn = psycopg2.connect(host="localhost", port="5432", user="postgres", password="postgres", database="postgres")
+    cur = conn.cursor()
+    
+    cur.execute(create_patients_table)
+    cur.execute(create_providers_table)
+    conn.commit()
+
+    PATIENTS_CSV_FILE = 'data/patients/patients.csv'
+    with open(PATIENTS_CSV_FILE, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            cur.execute(insert_into_patients_table, [row['patient_id'], row['first_name'], row["last_name"], row['age']])
+    conn.commit()
+
+    PROVIDERS_CSV_FILE = 'data/providers/providers.csv'
+    with open(PROVIDERS_CSV_FILE, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            cur.execute(insert_into_providers_table, [row['provider_id'], row['first_name'], row["last_name"], row['title'], row['department']])
+    conn.commit()
 
 
-
-
-insert_into_patients_table="""INSERT INTO patients(patient_id, first_name, last_name, age) VALUES(%s,%s,%s, %s);"""
-PATIENTS_CSV_FILE = 'data/patients/patients.csv'
-with open(PATIENTS_CSV_FILE, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-
-    for row in reader:
-        cur.execute(insert_into_patients_table, [row['patient_id'], row['first_name'], row["last_name"], row['age']])
-conn.commit()
-
-        # dbsession.execute(insert_provider_department, [row['patient_id'], row['first_name'], row['last_name']])
-
-
-
-PROVIDERS_CSV_FILE = 'data/providers/providers.csv'
-insert_into_providers_table="""INSERT INTO providers(provider_id, first_name, last_name, title, department) VALUES(%s,%s,%s,%s,%s);"""
-with open(PROVIDERS_CSV_FILE, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-
-    for row in reader:
-        cur.execute(insert_into_providers_table, [row['provider_id'], row['first_name'], row["last_name"], row['title'], row['department']])
-conn.commit()
-
-
-
-# if __name__ =='__main__':
-#     populate_patients_table()
+if __name__ =='__main__':
+    populate_tables()
